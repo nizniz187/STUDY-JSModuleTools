ad5ac42c84f46ef58e7dde03af9ebd809f8afa3f
Niz <nizniz187@gmail.com>
2019-05-21 18:24:05 +0800

    asset management sample

--- webpack-setup/src/index.js
+++ webpack-setup/src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import './styles/style.css';
+import GIF from './images/dancing-banana.gif';
 
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
+    const image = new Image();
+    image.src = GIF;
+
+    element.appendChild(image);
     return element;
   }
   
