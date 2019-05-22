c15fcd3b39f49d8a0e75addacc6e2474a7aafb21
Niz <nizniz187@gmail.com>
2019-05-22 14:37:44 +0800

    Development Guide sample code

--- webpack-output-management/webpack.config.js
+++ webpack-output-management/webpack.config.js
@@ -3,15 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     // filename: 'main.js',
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Asset Management'
+      title: 'Output Management'
     }),
     new CleanWebpackPlugin()
   ],
